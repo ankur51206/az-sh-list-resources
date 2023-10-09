cc7ae54af2dd61ded741b7b96cdd0cf2e82c3c9a
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2023-10-09 20:06:01 +0530

    Update lists.sh

--- lists.sh
+++ lists.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-az account set --subscription "xxxxxxxxx-xxx-xxxx-xxxx-xxxxxxxxx"
+# Set the output file path
+output_file="resource_list.csv"
 
-# Define column widths
-rg_width=30
-resource_name_width=40
-resource_type_width=40
-resource_kind_width=20
+az account set --subscription "xxxxx-xxx-xxxx-xxxx-xxxxxxxx"
 
-# Print the table headers
-printf "%-${rg_width}s %-${resource_name_width}s %-${resource_type_width}s %-${resource_kind_width}s\n" "Resource Group" "Resource Name" "Resource Type" "Resource Kind"
+# Define column headers
+header="Resource Group,Resource Name,Resource Type,Resource Kind"
 
-# Separator line
-printf "%-${rg_width}s %-${resource_name_width}s %-${resource_type_width}s %-${resource_kind_width}s\n" "------------------" "----------------" "----------------" "----------------"
+# Initialize the output file with headers
+echo "$header" > "$output_file"
 
 # Get a list of all resource groups
 resourceGroups=$(az group list --query '[].name' --output tsv)
@@ -23,11 +20,13 @@ for rg in $resourceGroups; do
     # Get a list of resources in the current resource group
     resources=$(az resource list --resource-group $rg --query '[].{ResourceGroup:resourceGroup, Name:name, Type:type, Kind:kind}' --output tsv)
 
-    # Iterate through each resource and display it in tab-separated format
+    # Iterate through each resource and append it to the output file in CSV format
     while read -r line; do
         resource_name=$(echo "$line" | awk -F$'\t' '{print $2}') # Extract resource name from the line
         resource_type=$(echo "$line" | awk -F$'\t' '{print $3}') # Extract resource type from the Type column
         resource_kind=$(echo "$line" | awk -F$'\t' '{split($3, a, "/"); print a[length(a)]}') # Extract the last segment as resource kind
-        printf "%-${rg_width}s %-${resource_name_width}s %-${resource_type_width}s %-${resource_kind_width}s\n" "$rg" "$resource_name" "$resource_type" "$resource_kind"
+        echo "$rg,$resource_name,$resource_type,$resource_kind" >> "$output_file"
     done <<< "$resources"
 done
+
+echo "Output saved to $output_file"
